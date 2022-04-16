ddc61afebc7e266f72d7c54c42352915e927d713
Evgeniy Burdin <e.s.burdin@yandex.ru>
2022-04-16 12:04:10 +0400

    Упростил хранилище

--- core/storages/abstract.py
+++ core/storages/abstract.py
@@ -1,11 +1,19 @@
 """ Модуль для класса абстрактного хранилища.
 """
 from abc import ABC, abstractmethod
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 
 class AbstractStorage(ABC):
 
     @abstractmethod
-    def read(self) -> List[Dict[str, Any]]:
+    async def create(
+        self, table_name: str, row: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    async def read(
+        self, table_name: str, ids: Optional[list] = None
+    ) -> List[Dict[str, Any]]:
         pass
--- core/storages/simple.py
+++ core/storages/simple.py
@@ -1,11 +1,10 @@
 """ Модуль для класса простого хранилища на основе словаря.
 """
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional
 from uuid import uuid4
 
 from .abstract import AbstractStorage
 from .exception import StorageTableDoesNotExistError
-
 
 ID_FIELD_NAME = "id"
 
@@ -14,7 +13,9 @@ class SimpleStorage(AbstractStorage):
 
     def __init__(self) -> None:
 
-        self.db: Dict[str, List[Dict[str, Any]]] = {}
+        self.db: Dict[str, List[Dict[str, Any]]] = {
+            "articles": [],
+        }
 
     async def create(
         self, table_name: str, row: Dict[str, Any]
@@ -23,25 +24,21 @@ class SimpleStorage(AbstractStorage):
         if ID_FIELD_NAME not in row:
             row[ID_FIELD_NAME] = uuid4()
 
-        if table_name not in self.db:
-            self.db[table_name] = []
-
         self.db[table_name].append(row)
 
         return row
 
     async def read(
-        self, table_name: str, ids: Union[None, list] = None
+        self, table_name: str, ids: Optional[list] = None
     ) -> List[Dict[str, Any]]:
 
-        table_data = self.try_get_table_data(table_name)
+        table_data = self._try_get_table_data(table_name)
 
-        if ids is None:
-            return table_data
+        return table_data if ids is None else [
+            row for row in table_data if row[ID_FIELD_NAME] in ids
+        ]
 
-        return [row for row in table_data if row[ID_FIELD_NAME] in ids]
-
-    def try_get_table_data(self, table_name: str) -> List[Dict[str, Any]]:
+    def _try_get_table_data(self, table_name: str) -> List[Dict[str, Any]]:
 
         result = self.db.get(table_name)
 
