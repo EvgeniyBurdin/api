1f073cb884435fe0f5ce6d3b7ca8d3d6690e12fb
Evgeniy Burdin <e.s.burdin@yandex.ru>
2022-04-16 21:39:46 +0400

    Добавляет простую фильтрацию в метод чтения из хранилища

--- core/data_classes/results.py
+++ core/data_classes/results.py
@@ -1,6 +1,7 @@
 """ Модуль для классов данных результатов.
 """
-from uuid import UUID
+import datetime
+import uuid
 
 from pydantic import Field, StrictStr
 
@@ -10,6 +11,7 @@ from .base import BaseDC
 class ArticleResultsDC(BaseDC):
     """ Данные о статье.
     """
-    id: UUID = Field(description="Идентификатор")
+    id: uuid.UUID = Field(description="Идентификатор")
+    created: datetime.date = Field(description="Дата создания")
     header: StrictStr = Field(description="Заголовок")
     content: StrictStr = Field(description="Содержание")
--- core/storages/simple.py
+++ core/storages/simple.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 from .abstract import AbstractStorage
 from .exception import StorageTableDoesNotExistError
 
-ID_FIELD_NAME = "id"
+_ID_FIELD_NAME = "id"
 
 
 class SimpleStorage(AbstractStorage):
@@ -19,8 +19,7 @@ class SimpleStorage(AbstractStorage):
         self, table_name: str, row: Dict[str, Any]
     ) -> Dict[str, Any]:
 
-        if ID_FIELD_NAME not in row:
-            row[ID_FIELD_NAME] = uuid4()
+        row[_ID_FIELD_NAME] = uuid4()
 
         if table_name not in self.db:
             self.db[table_name] = []
@@ -30,14 +29,32 @@ class SimpleStorage(AbstractStorage):
         return row
 
     async def read(
-        self, table_name: str, ids: Optional[list] = None
+        self, table_name: str, filters: Optional[Dict[str, list]] = None
     ) -> List[Dict[str, Any]]:
 
         table_data = self._try_get_table_data(table_name)
 
-        return table_data if ids is None else [
-            row for row in table_data if row[ID_FIELD_NAME] in ids
-        ]
+        if filters is None:
+            return table_data
+
+        filtered_table_data = self._apply_filters(table_data, filters)
+
+        return filtered_table_data
+
+    def _apply_filters(
+        self, table_data: List[Dict[str, Any]], filters: Dict[str, list]
+    ) -> List[Dict[str, Any]]:
+
+        result = []
+
+        for row in table_data:
+            for name, value in filters.items():
+                if name not in row or value != row[name]:
+                    break
+            else:
+                result.append(row)
+
+        return result
 
     def _try_get_table_data(self, table_name: str) -> List[Dict[str, Any]]:
 
